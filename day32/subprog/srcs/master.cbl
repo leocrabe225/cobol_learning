761f81c47b76b63ad3a99f7e440f9210076d84f1
Leocrabe225 <petitscrounchs@yahoo.fr>
2025-06-13 10:09:53 +0200

    Day 32 exercise (technically not finished), and polished re-usable Makefile

---
+++ day32/subprog/srcs/master.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. master.
+       AUTHOR. Leocrabe225.
+       DATE-WRITTEN. 12-06-2025 (fr).
+
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+
+           DISPLAY "Terry est très joli".
+
+           CALL "subprog".
+
+           STOP RUN.
---
+++ day32/subprog/srcs/subprog.cbl
@@ -0,0 +1,12 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. subprog.
+       AUTHOR. Leocrabe225.
+       DATE-WRITTEN. 12-06-2025 (fr).
+
+       DATA DIVISION.
+
+       PROCEDURE DIVISION.
+
+           DISPLAY "This is a subprog".
+
+           STOP RUN.
